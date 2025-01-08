e78502fb1e17183add3a955e1a3bb65a6f43534d
KubangPawis <xanderfernandezperegrin@gmail.com>
2025-01-08 23:32:14 +0800

    fix: resolve firebase/androidManifest issues

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,23 +1,18 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+void main() {
+  runApp(const Quode());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Quode extends StatelessWidget {
+  const Quode({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Firebase Setup',
-      home: Scaffold(
-        appBar: AppBar(title: Text('Firebase Initialized')),
-        body: Center(child: Text('Firebase Setup Complete!')),
-      ),
+      title: 'Quode',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
     );
   }
 }
