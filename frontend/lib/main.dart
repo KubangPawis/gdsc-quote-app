3d6b0bb60cc05c5629b745138d36a8b66ffa1193
Rainzle Estuesta <rjestuesta@gmail.com>
2025-01-10 01:22:55 +0800

    feat: added auth services

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/pages/log_in_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:frontend/services/auth_services.dart'; // Your auth service file
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const Quode());
 }
 
