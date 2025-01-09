9512817ed2f7ceed9ca1e2b4c612680d623b93f9
clemenex <clemenexcortex@gmail.com>
2025-01-09 15:55:33 +0800

    feat: add sign up page and assets

--- frontend/lib/pages/sign_up_page.dart
+++ frontend/lib/pages/sign_up_page.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
+  _SignUpPageState createState() => _SignUpPageState(); 
+}
+
+class _SignUpPageState extends State<SignUpPage> {
   final _formKey = GlobalKey<FormState>();
+  bool _obscurePassword = true;
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
+          backgroundColor: Colors.white,
           title: Padding(
             padding: EdgeInsets.symmetric(horizontal: 20.0),
             child: Text("Sign Up", 
@@ -47,7 +54,13 @@ class SignUpPage extends StatelessWidget {
               controller: _emailController,
               decoration: InputDecoration(
                 labelText: 'Email Address',
-                border: OutlineInputBorder(),
+                labelStyle: TextStyle(color: Color(0xFF888888)),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFF888888), width: 1.0)
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFF283FB1), width: 1.0)
+                )
               ),
               keyboardType: TextInputType.emailAddress,
               validator: (value) {
@@ -59,19 +72,30 @@ class SignUpPage extends StatelessWidget {
             ),
             SizedBox(height: 20.0),
             TextFormField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
+                obscureText: _obscurePassword,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  labelStyle: TextStyle(color: Color(0xFF888888)),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF888888), width: 1.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF283FB1), width: 2.0),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscurePassword ? Icons.visibility_off : Icons.visibility,
+                      color: Color(0xFF888888),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscurePassword = !_obscurePassword;
+                      });
+                    },
+                  ),
+                ),
+                cursorColor: Color(0xFF283FB1)
               ),
-              obscureText: true,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a password';
-                }
-                return null;
-              },
-            ),
             SizedBox(height: 20.0),
             Center(
               child: SizedBox(
@@ -95,7 +119,57 @@ class SignUpPage extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     color: Color(0xFFFFFFFF)
                   )), 
-              )))
+              ))),
+              SizedBox(height: 20.0),
+              Row(
+                children: [
+                  Expanded(
+                    child: Divider(color: Color(0xFF888888), thickness: 1.0,),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text(
+                      "Or Sign Up With",
+                      style: TextStyle(
+                        fontFamily: 'Raleway',
+                        fontSize: 12,
+                        fontWeight: FontWeight.normal,
+                        color: Color(0xFF888888)
+                      )
+                    )),
+                  Expanded(
+                    child: Divider(color: Color(0xFF888888), thickness: 1.0),
+                  )
+                ],
+              ),
+              SizedBox(height: 20.0),
+              Center(
+                child: Flexible(
+                  child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    side: BorderSide(color: Color(0xFF888888), width: 1.0),
+                    padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0)
+                  ),
+                  onPressed: () {
+                    print("Google Sign-Up Clicked");
+                  },
+                  icon: Image.asset(
+                    'lib/assets/iconGoogle.png',
+                    height: 16.0,
+                    width: 16.0,
+                  ),
+                  label: Text(
+                    "Google",
+                    style: TextStyle(
+                      fontFamily: 'Raleway',
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.black
+                    )
+                  )
+                )
+              ))
             ],
           )
         )
