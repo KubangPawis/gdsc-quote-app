ebe29ca6cd892316d0fe22442bb6a2601642e9f9
KubangPawis <xanderfernandezperegrin@gmail.com>
2025-01-09 22:27:17 +0800

    feat(explore): add container widget

--- frontend/lib/pages/explore_page.dart
+++ frontend/lib/pages/explore_page.dart
@@ -1,22 +1,44 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-class ExplorePage extends StatelessWidget {
+class ExplorePage extends StatefulWidget {
   const ExplorePage({super.key});
+
+  @override
+  State<ExplorePage> createState() => _ExplorePageState();
+}
+
+class _ExplorePageState extends State<ExplorePage> {
+  final List quoteWidgetColorList = [
+    Color(0xFFFFE0E0),
+    Color(0xFFC4F1E3),
+    Color(0xFFDAF8FF),
+    Color(0xFFFFF8D1),
+    Color(0xFFCA9FED),
+    Color(0xFF686CFC),
+    Color(0xFFFFBD94),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.all(12),
-        child: Container(
-            child: Column(
-          children: [
+      padding: EdgeInsets.all(24),
+      child: Container(
+          decoration: BoxDecoration(
+            color: quoteWidgetColorList[
+                Random().nextInt(quoteWidgetColorList.length)],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
             Text(
               'It is what is it.',
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
             ),
             Text(
               'Sun Tzu',
+              style: TextStyle(fontSize: 16, color: Colors.black54),
             ),
-          ],
-        )));
+          ])),
+    );
   }
 }
